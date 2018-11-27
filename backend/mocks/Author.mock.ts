23822c5ed4a0d7e14bf651f99907f75d834057df
Johannes Kling <johannes.kling@tui.com>
2018-11-27 11:05:55 +0100

    added mock data

--- backend/mocks/Author.mock.ts
+++ backend/mocks/Author.mock.ts
@@ -1,13 +1,31 @@
-import { IAuthor } from '../model/Author';
+import {IAuthor} from '../model/Author';
 
 export const AuthorMocks: IAuthor[] = [
-  {
-    displayName: "Stephen",
-    firstName: "Stephen",
-    lastName: "King",
-    joinedTimestamp: 1543168679694,
-    avatarUrl: "https://source.unsplash.com/random",
-    id: "1",
-    email: "stephen@king.com"
-  }
+    {
+        displayName: "stevie",
+        firstName: "Stephen",
+        lastName: "King",
+        joinedTimestamp: 1543168679694,
+        avatarUrl: "https://source.unsplash.com/random",
+        id: "1",
+        email: "stephen@king.com"
+    },
+    {
+        displayName: "rowling",
+        firstName: "Joanne",
+        lastName: "Rowling",
+        joinedTimestamp: 1543168679694,
+        avatarUrl: "https://source.unsplash.com/random",
+        id: "2",
+        email: "harry@potter.com"
+    },
+    {
+        displayName: "jibbedi",
+        firstName: "Johannes",
+        lastName: "Kling",
+        joinedTimestamp: 1543168679694,
+        avatarUrl: "https://source.unsplash.com/random",
+        id: "3",
+        email: "johannes.kling92@gmail.com"
+    }
 ];
